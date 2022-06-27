c86148707bef86c7393a0eb241990d497b85a501
Julia Majkowski <77689607+juliamajkowski@users.noreply.github.com>
2022-06-27 10:41:26 -0400

    Add files via upload

---
+++ JuliaMajkowskiProject1/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using TripLog.Models;
+
+namespace TripLog
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting(options => {
+                options.LowercaseUrls = true;
+                options.AppendTrailingSlash = true;
+            });
+
+            services.AddControllersWithViews();
+
+            services.AddDbContext<TripContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("TripContext")));
+        }
+
+        // Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}");
+            });
+        }
+    }
+}
+
